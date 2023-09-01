bba1affe4cfbda3e72fe1d8b855109af558a660c
Alex Wiehl <alexwiehl@gmail.com>
2023-09-01 00:14:50 -0400

    Add submit hover state

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 function Button({ label }) {
   return (
-    <button className="border border-black rounded-full w-[25%] bg-[#1B49A6] min-w-[100px] text-white mx-2">
+    <button className="border border-black rounded-full w-[25%] bg-[#f06735] min-w-[100px] text-white mx-2 hover:bg-[#1B49A6] transition-all ease-in-out duration-200">
       {label}
     </button>
   );
